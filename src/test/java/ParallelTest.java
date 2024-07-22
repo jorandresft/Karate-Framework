d085310718c75c1bda6ba5325cf941c26bf45d6f
jft85 <jorgeandresft@gmail.com>
2024-07-22 14:45:01 -0500

    Realizando el primer commit

---
+++ src/test/java/ParallelTest.java
@@ -0,0 +1,2 @@
+public class ParallelTest {
+}
---
+++ src/test/java/geonames/get/GeographicalDataGetRunner.java
@@ -0,0 +1,12 @@
+package geonames.get;
+
+import com.intuit.karate.junit5.Karate;
+
+public class GeographicalDataGetRunner {
+
+    @Karate.Test
+    Karate geographicalDataGet(){
+        return Karate.run().relativeTo(getClass());
+    }
+
+}
